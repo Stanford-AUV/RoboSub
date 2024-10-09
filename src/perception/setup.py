2c876e59521f072af6e8298d2aa53bb4cd7ff6d6
TomoCoder123 <thomasxdeng@gmail.com>
2024-10-09 04:43:26 +0000

    Copied and pasted Perception code from last year

--- src/perception/setup.py
+++ src/perception/setup.py
@@ -1,25 +1,27 @@
 from setuptools import find_packages, setup
 
-package_name = 'perception'
+package_name = "perception"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ros',
-    maintainer_email='scotthickmann21@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="Stanford Robosub",
+    maintainer_email="selenasun02@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            f"perception_node = {package_name}.perception_node:main",
+            f"debug_node = {package_name}.debug_node:main",
+            f"data_node = {package_name}.data_node:main",
         ],
     },
 )
