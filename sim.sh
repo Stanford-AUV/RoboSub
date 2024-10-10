dc0c98df45f69761eae93b86ee398e30f60226bb
Scott Hickmann <scotthickmann21@gmail.com>
2024-10-09 22:49:15 -0700

    Port simulation code from last year (#5)

--- sim.sh
+++ sim.sh
@@ -1,1 +1,4 @@
-GZ_PARTITION=127.0.0.1:ros gz sim src/simulation/simulation/models/world.urdf -s
+export GZ_PARTITION=127.0.0.1:ros
+gz sim src/simulation/simulation/models/world.urdf -s -r \
+& ros2 run ros_gz_bridge parameter_bridge --ros-args -p config_file:=src/simulation/simulation/gazebo_bridge.yaml \
+& ros2 launch src/launch/simulation.py
--- src/simulation/setup.py
+++ src/simulation/setup.py
@@ -1,25 +1,26 @@
 from setuptools import find_packages, setup
 
-package_name = 'simulation'
+package_name = "simulation"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ros',
-    maintainer_email='scotthickmann21@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="ros",
+    maintainer_email="scotthickmann21@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "thrusters = simulation.nodes.thrusters:main",
+            "sensors = simulation.nodes.sensors:main",
         ],
     },
 )
