9e7b5c31ebf54b9557d1a05d629710728a0f9e68
Rohan Tan Bhowmik <rbhowmik@stanford.edu>
2024-10-10 22:06:34 -0700

    (WIP) 3d Rotation draft

--- src/planning/planning/utils/path_3d_rotation.py
+++ src/planning/planning/utils/path_3d_rotation.py
@@ -1,18 +1,29 @@
+import matplotlib
+matplotlib.use('Agg')
+
 import numpy as np
 from scipy.interpolate import make_interp_spline
-from scipy.spatial.transform import Rotation, RotationSpline
+from scipy.spatial.transform import Rotation#, RotationSpline
+
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.interpolate import CubicSpline
 
 # Example points (x, y)
-x = np.random.rand(5)
-y = np.random.rand(5)
-z = np.random.rand(5)
-orientations = Rotation.random(5).as_euler("xyz", degrees=True)
+x = np.random.rand(2)
+y = np.random.rand(2)
+z = np.random.rand(2)
+# orientations = Rotation.random(5).as_euler("xyz", degrees=True)
 x = np.concatenate((x, [x[0]]))
 y = np.concatenate((y, [y[0]]))
 z = np.concatenate((z, [z[0]]))
+# orientations = np.concatenate((orientations, [orientations[0]]))
+
+arr = np.linspace(-180, 180, 1)
+# orientations = np.array([[-179.99, -179.99, -179.99], [179.99, 179.99, 179.99]])
+# orientations = np.array([[-1, -1, -1], [1, 1, 1]])
+# orientations = np.array([[-149.99, -149.99, -149.99], [149.99, 149.99, 149.99]])
 orientations = np.concatenate((orientations, [orientations[0]]))
 
 # Parameter t (can be the distance between the points or just range of integers)
@@ -31,25 +42,81 @@ splines = [
 # Define a finer grid of t values for evaluation
 t_fine = np.linspace(0, 1, 100)
 
-orientation_spline = RotationSpline(
-    t, Rotation.from_euler("xyz", orientations, degrees=True)
-)
+class RotationSpline:
+    def __init__(self, t, quaternions):
+
+        assert len(t) == len(quaternions)
+        
+        self.t = t
+        self.quaternions = quaternions
+
+        self.rotations = Rotation.from_quat(quaternions)
+        self.rotvecs = self.rotations.as_rotvec()
+
+        print(self.rotvecs)
+
+        self.spline_x = make_interp_spline(t, self.rotvecs[:, 0], k=5, bc_type=([(1, 0.0), (2, 0.0)], [(1, 0.0), (2, 0.0)]))
+        self.spline_y = make_interp_spline(t, self.rotvecs[:, 1], k=5, bc_type=([(1, 0.0), (2, 0.0)], [(1, 0.0), (2, 0.0)]))
+        self.spline_z = make_interp_spline(t, self.rotvecs[:, 2], k=5, bc_type=([(1, 0.0), (2, 0.0)], [(1, 0.0), (2, 0.0)]))
+
+
+    def __call__(self, t_single):
+        t_array = np.array([t_single])
+        quaternions_fine = self.evaluate(t_array)
+        return quaternions_fine[0]
+
+    def evaluate(self, t_fine):
+        rotvecs_fine = np.vstack((
+            self.spline_x(t_fine),
+            self.spline_y(t_fine),
+            self.spline_z(t_fine)
+        )).T
+        quaternions_fine = Rotation.from_rotvec(rotvecs_fine).as_quat()
+        return quaternions_fine
+
+    def as_euler(self, t_fine, order='xyz', degrees=False):
+        quaternions_fine = self.evaluate(t_fine)
+        rotations = Rotation.from_quat(quaternions_fine)
+        return rotations.as_euler(order, degrees=degrees)
+
+    def angular_velocity(self, t_fine):
+        angular_vel_x = self.spline_x(t_fine, 1)
+        angular_vel_y = self.spline_y(t_fine, 1)
+        angular_vel_z = self.spline_z(t_fine, 1)
+        return np.vstack((angular_vel_x, angular_vel_y, angular_vel_z)).T
+
+    def angular_acceleration(self, t_fine):
+        angular_acc_x = self.spline_x(t_fine, 2)
+        angular_acc_y = self.spline_y(t_fine, 2)
+        angular_acc_z = self.spline_z(t_fine, 2)
+        return np.vstack((angular_acc_x, angular_acc_y, angular_acc_z)).T
+    
+# orientation_spline = RotationSpline(
+#     t, Rotation.from_euler("xyz", orientations, degrees=True)
+# )
+
+quaternion_spline = RotationSpline(t, Rotation.from_euler("xyz", orientations, degrees=True).as_quat())
+
+# For angular velocities and accelerations:
+angular_velocities = quaternion_spline.angular_velocity(t_fine)
+angular_accelerations = quaternion_spline.angular_acceleration(t_fine)
 
 # Compute positions
 positions = [spline(t_fine) for spline in splines]
 x_fine, y_fine, z_fine = positions
-orientations = orientation_spline(t_fine).as_euler("xyz", degrees=True)
+# orientations = orientation_spline(t_fine).as_euler("xyz", degrees=True)
+orientations = quaternion_spline.as_euler(t_fine, order='xyz', degrees=True)
+
 
 # Compute velocities (first derivative)
 velocities = [spline(t_fine, 1) for spline in splines]
 v_x, v_y, v_z = velocities
-angular_velocities = orientation_spline(t_fine, 1)
-
+angular_velocities = quaternion_spline.angular_velocity(t_fine) #orientation_spline(t_fine, 1)
 
 # Compute accelerations (second derivative)
 accelerations = [spline(t_fine, 2) for spline in splines]
 a_x, a_y, a_z = accelerations
-angular_accelerations = orientation_spline(t_fine, 2)
+angular_accelerations = quaternion_spline.angular_acceleration(t_fine) #orientation_spline(t_fine, 2)
 
 # Plot the results
 plt.figure(figsize=(10, 8))
@@ -112,6 +179,8 @@ plt.ylabel("Angular Acceleration")
 plt.tight_layout()
 plt.show()
 
+plt.savefig("plot.png")
+
 
 # Setup the figure and 3D axis
 fig = plt.figure()
@@ -135,7 +204,7 @@ ax.legend()
 # Function to draw orientation (as a line or arrow)
 def draw_orientation(position, orientation):
     """Draws the orientation of the vehicle at a given position using the quaternion."""
-    direction = orientation.apply([1, 0, 0])
+    direction = Rotation.from_euler('xyz', orientation).apply([1, 0, 0])
     end_point = position + 0.2 * direction  # Scale for visualization
     return position, end_point
 
@@ -159,7 +228,7 @@ def animate(i):
 
     # Get the current quaternion and position
     position = np.array([x_fine[i], y_fine[i], z_fine[i]])
-    orientation = orientation_spline(t_fine[i])
+    orientation = Rotation.from_quat(quaternion_spline(t_fine[i])).as_euler('xyz')
 
     # Draw orientation line
     start, end = draw_orientation(position, orientation)
@@ -173,6 +242,8 @@ def animate(i):
 anim = FuncAnimation(
     fig, animate, init_func=init, frames=len(t_fine), interval=20, blit=True
 )
+
+anim.save('animation.mp4', writer='ffmpeg')
 
 # Show the animation
 plt.show()
