6772eb8a4f096b055a60ea76ac56ae607f0753b5
AliAhmad810 <hiimaliahmad@gmail.com>
2024-10-12 10:30:50 +0000

    Callback functions

--- src/control/control/nodes/controller.py
+++ src/control/control/nodes/controller.py
@@ -1,14 +1,25 @@
 """
 Controller Node.
 
+Dependencies:
+    geometry_msgs.msg.WrenchStamped
+    nav_msgs.msg.Odometry
+    numpy
+    rclpy
+    rclpy.node.Node
+    threading
+
 Authors:
     Ali Ahmad
     Khaled Messai
 
-
+Version:
+    1.0.0
 """
 
 import threading
+
+from control.utils.utils import to_np
 
 from geometry_msgs.msg import WrenchStamped
 
@@ -18,18 +29,19 @@ import numpy as np
 
 import rclpy
 from rclpy.node import Node
+from rclpy.time import Time
 
 
 class Controller(Node):
     """
     Node that computes a control signal based on state and reference poses.
 
-    Creates subscribers to the state and reference topics and publishes to the 
-    desired_wrench topic. Uses a PID controller to compute the control signal 
+    Creates subscribers to the state and reference topics and publishes to the
+    desired_wrench topic. Uses a PID controller to compute the control signal
     with configurable gains and limits.
     """
 
-    def __init__(self, k_pos, k_ang, ceil):
+    def __init__(self, k_pos: np.array, k_ang: np.array, ceil: np.array):
         """
         Initialize the controller node.
 
@@ -69,18 +81,33 @@ class Controller(Node):
         self.k_ang = np.diag(k_ang)
         self.max = ceil
 
+        self.state = None
+        self.reference = None
 
-    def state_callback(self):
-        pass
+    def state_callback(self, msg: Odometry):
+        with self.lock:
+            self.state = to_np(msg)
+            timestamp = Time(
+                seconds=msg.header.stamp.sec,
+                nanoseconds=msg.header.stamp.nanosec,
+                clock_type=self.get_clock().clock_type
+            )
+            self.dt = (timestamp - self.time).nanoseconds / 1e6
+            self.time = timestamp
+            self.update()
 
-    def reference_callback(self):
-        pass
+    def reference_callback(self, msg: Odometry):
+        with self.lock:
+            self.reference = to_np(msg)
 
     def update(self):
-        pass
+        r_W = self.state[0:3]  # position, world frame
+        
 
     def reset(self):
-        pass
+        self.get_logger.info('Resetting controller...')
+        with self.lock:
+            self.integral = np.zeros(6)
 
 
 def main(args=None):
