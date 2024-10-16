4558bffdc5a5bea788d078df12ca1e6c805a0399
AliAhmad810 <hiimaliahmad@gmail.com>
2024-10-16 03:54:15 +0000

    push so far

--- src/control/control/nodes/controller.py
+++ src/control/control/nodes/controller.py
@@ -5,13 +5,14 @@ Dependencies:
     geometry_msgs.msg.WrenchStamped
     nav_msgs.msg.Odometry
     numpy
+    quaternion
     rclpy
     rclpy.node.Node
+    rclpy.time.Time
     threading
 
 Authors:
     Ali Ahmad
-    Khaled Messai
 
 Version:
     1.0.0
@@ -19,13 +20,15 @@ Version:
 
 import threading
 
-from control.utils.utils import to_np
+from control.utils.utils import quat_to_axis_angle, qvmul, to_np
 
 from geometry_msgs.msg import WrenchStamped
 
 from nav_msgs.msg import Odometry
 
 import numpy as np
+
+import quaternion
 
 import rclpy
 from rclpy.node import Node
@@ -41,7 +44,12 @@ class Controller(Node):
     with configurable gains and limits.
     """
 
-    def __init__(self, k_pos: np.array, k_ang: np.array, ceil: np.array):
+    def __init__(self,
+                 kp: np.array,
+                 kd: np.array,
+                 ki: np.array,
+                 max_signal: np.ndarray,
+                 max_i: np.ndarray):
         """
         Initialize the controller node.
 
@@ -52,14 +60,16 @@ class Controller(Node):
 
         Parameters
         ----------
-        k_pos: np.array (3x1)
-            The position PID controller gains.
-        k_ang: np.array (3x1)
-            The angular PID controller gains.
-        ceil: np.array (2x1)
+        kp: np.ndarray (2x3)
+            The proportional gains for the controller (pos, ang).
+        kd: np.ndarray (2x3)
+            The derivative gains for the controller (pos, ang).
+        ki: np.ndarray (2x3)
+            The integral gains for the controller (pos, ang).
+        ceil: np.ndarray (2x3)
             The saturation limits for the controller.
-        start_i: np.array (2x1)
-            The initial integral error for the controller.
+        max_i: np.ndarray (2x3)
+            The integral saturation limits for the controller.
         """
         super().__init__('controller')
 
@@ -77,12 +87,19 @@ class Controller(Node):
 
         self.time = self.get_clock().now()
 
-        self.k_pos = np.diag(k_pos)
-        self.k_ang = np.diag(k_ang)
-        self.max = ceil
+        self.kP_pos = kp[0]
+        self.kP_ang = kp[1]
+        self.kD_pos = kd[0]
+        self.kD_ang = kd[1]
+        self.kI_pos = ki[0]
+        self.kI_ang = ki[1]
+        self.max_signal = max_signal
+        self.max_i = max_i
 
         self.state = None
         self.reference = None
+        self.integral_pos = 0
+        self.integral_ang = 0
 
     def state_callback(self, msg: Odometry):
         with self.lock:
@@ -102,7 +119,42 @@ class Controller(Node):
 
     def update(self):
         r_W = self.state[0:3]  # position, world frame
-        
+        v_B = self.state[3:6]  # velocity, body frame
+        q_W = quaternion(self.state[6:10])  # orientation, world frame
+        w_B = self.state[10:13]  # angular velocity, body frame
+
+        r_W_ref = self.reference[0:3]  # position, world frame
+        v_B_ref = self.reference[3:6]  # velocity, body frame
+        q_W_ref = quaternion(self.reference[6:10])  # orientation, world frame
+        w_B_ref = self.reference[10:13]  # angular velocity, body frame
+
+        # pose error
+        error_r_W = r_W_ref - r_W
+
+        q_W_inv = np.conjugate(q_W)
+        q_W_ref_inv = np.conjugate(q_W_ref)
+        error_q_W = q_W_inv * q_W_ref_inv
+        error_q_W = quat_to_axis_angle(error_q_W)
+
+        # velocity error
+        error_v_B = v_B_ref - v_B
+        error_w_B = w_B_ref - w_B
+
+        # integral term
+        self.integral_pos = np.clip(
+            (self.integral_pos + error_r_W + error_v_B) * self.dt,
+            -self.max_i[0],
+            self.max_i[0]
+        )
+        self.integral_ang = np.clip(
+            (self.integral_ang + error_q_W + error_w_B) * self.dt,
+            -self.max_i[1],
+            self.max_i[1]
+        )
+
+        # force on the body (in the body frame)
+        F_W = self.kP_pos * error_r_W + self.kD_pos * error_v_B + self.kI_pos * self.integral_pos
+        F_B = qvmul(q_W_inv, F_W)
 
     def reset(self):
         self.get_logger.info('Resetting controller...')
--- src/control/control/utils/utils.py
+++ src/control/control/utils/utils.py
@@ -5,14 +5,16 @@ Author:
     Ali Ahmad
 
 Version:
-    1.0.0
+    1.0.1
 """
 from nav_msgs.msg import Odometry
 
 import numpy as np
 
+import quaternion
 
-def to_np(o: Odometry):
+
+def to_np(o: Odometry) -> np.array:
     """Convert a ROS odometry message to a numpy array."""
     if isinstance(o, Odometry):
         return np.array([
@@ -30,4 +32,22 @@ def to_np(o: Odometry):
             o.twist.twist.angular.y,
             o.twist.twist.angular.z
         ])
-    raise ValueError("Unsupported type for toNp")
+    raise ValueError('Unsupported type for to_np')
+
+
+def quat_to_axis_angle(q: quaternion) -> np.array:
+    """Convert a quaternion to an axis-angle representation."""
+    q = q.normalized()
+    w = q.w
+    v = np.array([q.x, q.y, q.z])
+    theta = 2 * np.arccos(w)
+    normv = np.sqrt(1 - w**2)
+    axis = np.zeros(3) if normv < 1e-10 else v / normv
+    return axis * theta
+
+
+def qvmul(q: quaternion, v: np.array) -> np.array:
+    """Multiply a quaternion with a vector."""
+    q = q.normalized()
+    v = quaternion.quaternion(0, *v)
+    return (q * v * q.conjugate()).imaginary
