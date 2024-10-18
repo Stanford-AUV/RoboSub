5c756e8b166399297cf0c5b066a8a834514abc96
Isaiah Hall <isaiah03@stanford.edu>
2024-10-18 05:16:12 +0000

    created basic plugin which renders an object in the world

---
+++ src/simulation/simulation/custom_gz_plugins/PathVisualizer.cc
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2021 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gz/gui/Application.hh>
+//! [includeGuiEvents]
+#include <gz/gui/GuiEvents.hh>
+//! [includeGuiEvents]
+#include <gz/gui/MainWindow.hh>
+#include <gz/math/Rand.hh>
+#include <gz/plugin/Register.hh>
+#include <gz/rendering/RenderEngine.hh>
+#include <gz/rendering/RenderingIface.hh>
+#include <gz/rendering/Scene.hh>
+
+#include "PathVisualizer.hh"
+
+/////////////////////////////////////////////////
+void PathVisualizer::LoadConfig(const tinyxml2::XMLElement * /*_pluginElem*/)
+{
+  // This is necessary to receive the Render event on eventFilter
+//! [connectToGuiEvent]
+  gz::gui::App()->findChild<
+      gz::gui::MainWindow *>()->installEventFilter(this);
+//! [connectToGuiEvent]
+}
+
+/////////////////////////////////////////////////
+void PathVisualizer::TogglePath()
+{
+  this->dirty = true;
+  this->showCube = !this->showCube;  // Toggle cube visibility
+}
+
+/////////////////////////////////////////////////
+//! [eventFilter]
+bool PathVisualizer::eventFilter(QObject *_obj, QEvent *_event)
+{
+  if (_event->type() == gz::gui::events::Render::kType)
+  {
+    // This event is called in the render thread, so it's safe to make
+    // rendering calls here
+    this->PerformRenderingOperations();
+  }
+
+  // Standard event processing
+  return QObject::eventFilter(_obj, _event);
+}
+//! [eventFilter]
+
+/////////////////////////////////////////////////
+//! [performRenderingOperations]
+void PathVisualizer::PerformRenderingOperations()
+{
+  if (!this->dirty)
+  {
+    return;
+  }
+
+  if (nullptr == this->scene)
+  {
+    this->FindScene();
+  }
+
+  if (nullptr == this->scene)
+    return;
+
+  // Remove existing cube if any
+  if (this->cubeVisual)
+  {
+    this->scene->RootVisual()->RemoveChild(this->cubeVisual);
+    this->cubeVisual.reset();
+  }
+
+  // Create and add a new cube if showCube is true
+  if (this->showCube)
+  {
+    this->cubeVisual = this->scene->CreateVisual("cube_visual");
+    auto cubeMesh = this->scene->CreateBox();
+    this->cubeVisual->AddGeometry(cubeMesh);
+    this->cubeVisual->SetLocalPosition(5.0, 5.0, 1.0);  // Arbitrary position
+    this->scene->RootVisual()->AddChild(this->cubeVisual);
+  }
+
+  this->dirty = false;
+}
+//! [performRenderingOperations]
+
+/////////////////////////////////////////////////
+void PathVisualizer::FindScene()
+{
+  auto loadedEngNames = gz::rendering::loadedEngines();
+  if (loadedEngNames.empty())
+  {
+    gzdbg << "No rendering engine is loaded yet" << std::endl;
+    return;
+  }
+
+  // assume there is only one engine loaded
+  auto engineName = loadedEngNames[0];
+  if (loadedEngNames.size() > 1)
+  {
+    gzdbg << "More than one engine is available. "
+      << "Using engine [" << engineName << "]" << std::endl;
+  }
+  auto engine = gz::rendering::engine(engineName);
+  if (!engine)
+  {
+    gzerr << "Internal error: failed to load engine [" << engineName
+      << "]. Grid plugin won't work." << std::endl;
+    return;
+  }
+
+  if (engine->SceneCount() == 0)
+  {
+    gzdbg << "No scene has been created yet" << std::endl;
+    return;
+  }
+
+  // Get first scene
+  auto scenePtr = engine->SceneByIndex(0);
+  if (nullptr == scenePtr)
+  {
+    gzerr << "Internal error: scene is null." << std::endl;
+    return;
+  }
+
+  if (engine->SceneCount() > 1)
+  {
+    gzdbg << "More than one scene is available. "
+      << "Using scene [" << scene->Name() << "]" << std::endl;
+  }
+
+  if (!scenePtr->IsInitialized() || nullptr == scenePtr->RootVisual())
+  {
+    return;
+  }
+
+  this->scene = scenePtr;
+}
+
+// Register this plugin
+GZ_ADD_PLUGIN(PathVisualizer,
+                    gz::gui::Plugin);
