a20f74eef533e4ba414eea89b05d8a2cbbfa8d71
TomoCoder123 <76802324+TomoCoder123@users.noreply.github.com>
2024-10-24 21:07:36 -0700

    Tomocoder/perception detection2darray (#27)
    * [wip] detection2darray msg
    
    * Fixed typing errors in detection message!

--- src/perception/perception/nodes/YOLONode.py
+++ src/perception/perception/nodes/YOLONode.py
@@ -5,6 +5,15 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from ultralytics import YOLO
 import cv2
+from vision_msgs.msg import (
+    BoundingBox2D,
+    Detection2D,
+    Detection2DArray,
+    ObjectHypothesisWithPose,
+    Pose2D,
+)
+
+# from geometry_msgs.msg import Pose2D
 
 
 class YoloV8ROSNode(Node):
@@ -22,6 +31,7 @@ class YoloV8ROSNode(Node):
         self.subscription = self.create_subscription(
             Image, "oak/rgb/image_raw", self.image_callback, 10
         )
+        self.pub = self.create_publisher(Detection2DArray, "/detections2d", 10)
 
         # Log to indicate that the node has started
         self.get_logger().info(
@@ -35,7 +45,51 @@ class YoloV8ROSNode(Node):
         # Run YOLO model on the image
         results = self.model(cv_image)
 
+        def xywh_to_bbox(xywh_list):
+            [[x, y, w, h]] = xywh_list
+            # Calculate center coordinates
+            center_x = x + w / 2
+            center_y = y + h / 2
+
+            # Set size_x and size_y
+            size_x = float(w)
+            size_y = float(h)
+
+            # Create the Pose2D for the center
+            pose = Pose2D()
+            pose.position.x = float(center_x)
+            pose.position.y = float(center_y)
+
+            return {"center": pose, "size_x": size_x, "size_y": size_y}
+
+        detections_array = Detection2DArray()
+        # append detection (i.e. a bounding box) to detections_array
+        num_bbox = len(results[0].boxes.xywh)
+        for bbox_idx in range(num_bbox):
+            detection = Detection2D()
+
+            # init objecthypothsis...
+            objhypo = ObjectHypothesisWithPose()
+            objhypo.hypothesis.class_id = str(results[0].boxes[bbox_idx].cls)
+            objhypo.hypothesis.score = float(results[0].boxes[bbox_idx].conf)
+            detection.results.append(objhypo)
+
+            # init 2d boundingbox
+            xywh = results[0].boxes[bbox_idx].xywh
+            bbox_info = xywh_to_bbox(xywh)
+            detection.bbox.center = bbox_info["center"]
+            detection.bbox.size_x = bbox_info["size_x"]
+            detection.bbox.size_y = bbox_info["size_y"]
+
+            # (opt) init source image
+
+            # append detection to array
+            detections_array.detections.append(detection)
+
         # Annotate the image with detections
+        detections_array.header = msg.header
+        self.pub.publish(detections_array)
+
         annotated_frame = results[0].plot()
 
         # Display the annotated frame (optional)
