c7528f0af74eae8d27c3879dad9adf6937ec1812
Tōkaido-Sanyō-Express <derenji@yahoo.com>
2024-11-15 07:08:18 +0000

    updates nextState to nextReference

--- src/control/control/utils/nextReference.py
+++ src/control/control/utils/nextReference.py
@@ -5,18 +5,35 @@ import numpy as np
 #the index to be 0 if initially given less than 0, and it will be fixed at the max index when it's
 #at the end of path.
 
+#!!! IS THERE A .SIZE() METHOD FOR PATH TYPE???!!!
+
 class nextReference:
-    def __init__ (self, path, index):
+    def __init__ (self, cur_state, path):
+        self.cur_state = currentState
         self.path = path
-        self.index = index
+        self.index = 0
+        
     
     def initializer (self):
         self.path = None
         self.index = 0
 
-    def getNextReference (self, currentState):
+    #in progress
+    def changePath (self, newPath):
+        newIndex = self.index
+        if newIndex + 1 < newPath.size():
+            if newPath[newIndex]:
+        self.path = newPath
+
+    def changeState (self, newState):
+        self.cur_state = newState
+
+    def getNextReference (self):
         if index < 0:
             index = 0
+        
+        currentPoint = cur_state.pos
+        
         distanceToNext = sqrt((currentState.x - self.path[self.index].x) ** 2 + 
                         (currentState.y - self.path[self.index].y ** 2) + 
                         (currentState.z - self.path[self.index].z) ** 2)
@@ -27,3 +44,10 @@ class nextReference:
         if distanceToNext < distanceToPrevious and index < path.size() - 1:
             index += 1
         return path[self.index] 
+
+    def getDistance(point1, point2):
+        return sqrt((point1.x - point2.x) ** 2 + 
+                        (point1.y - point2.y ** 2) + 
+                        (point1.z - point2.z) ** 2)
+
+    
