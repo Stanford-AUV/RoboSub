0682f079b3578c9d23bee1340e1c3a19c33431a0
kl31415 <kevinli7@stanford.edu>
2024-11-15 06:50:06 +0000

    Fix camera node

--- src/perception/perception/nodes/test/view_video.py
+++ src/perception/perception/nodes/test/view_video.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -9,6 +10,7 @@ import numpy as np
 
 class ViewVideo(Node):
     detections: list[Detection2D]
+    frame_dims: Optional[tuple]
 
     def __init__(self):
         super().__init__("view_video")
@@ -25,11 +27,59 @@ class ViewVideo(Node):
             Detection2DArray, "detections2d", self.detections_2d_callback, 10
         )
         self.detections = []
+        self.frame_dims = None
 
     def rgb_callback(self, msg):
         # Convert ROS Image message to OpenCV image
         frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
+        self.frame_dims = frame.shape
+
+        # Overlay BBOX
+        self.overlay_bbox(frame)
+
+        # Display the frame
+        cv2.imshow("Video Stream", frame)
+
+        # Wait for a short time and check for the 'q' key to close the window
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            self.get_logger().info("Quitting video display")
+            rclpy.shutdown()
+
+    def depth_callback(self, msg):
+        if not self.frame_dims:
+            return
+
+        # Convert ROS Image message to OpenCV image (RGB representation of depth)
+        depth_frame = self.bridge.imgmsg_to_cv2(msg, "passthrough")
+
+        # Normalization for better visualization
+        ratio = (
+            255 / 95
+        )  # Can use cam_depth.initialConfig.getMaxDisparity() instead of 95
+        depth_frame_tmp = (depth_frame * ratio).astype(np.uint8)
+        depth_frame_color_map = cv2.applyColorMap(depth_frame_tmp, cv2.COLORMAP_JET)
+        self.get_logger().info(f"Shape: {depth_frame_color_map.shape}")
+        self.get_logger().info(f"Shape: {self.frame_dims}")
+        depth_frame_color_map = cv2.resize(
+            depth_frame_color_map, (self.frame_dims[1], self.frame_dims[0])
+        )
+
+        # Overlay BBOX
+        self.overlay_bbox(depth_frame_color_map)
+
+        # Display the depth frame
+        cv2.imshow("Depth Video Stream", depth_frame_color_map)
+
+        # Quit if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            self.get_logger().info("Quitting depth display")
+            rclpy.shutdown()
+
+    def detections_2d_callback(self, msg: Detection2DArray):
+        self.detections = msg.detections
+
+    def overlay_bbox(self, frame: np.ndarray):
         # Draw bounding boxes and labels on the frame
         for detection in self.detections:
             bbox = detection.bbox
@@ -55,36 +105,6 @@ class ViewVideo(Node):
                     (0, 255, 0),
                     2,
                 )
-
-        # Display the frame
-        cv2.imshow("Video Stream", frame)
-
-        # Wait for a short time and check for the 'q' key to close the window
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            self.get_logger().info("Quitting video display")
-            rclpy.shutdown()
-
-    def depth_callback(self, msg):
-        # Convert ROS Image message to OpenCV image (RGB representation of depth)
-        depth_frame = self.bridge.imgmsg_to_cv2(msg, "passthrough")
-
-        # Normalization for better visualization
-        ratio = (
-            255 / 95
-        )  # Can use cam_depth.initialConfig.getMaxDisparity() instead of 95
-        depth_frame_tmp = (depth_frame * ratio).astype(np.uint8)
-        depth_frame_color_map = cv2.applyColorMap(depth_frame_tmp, cv2.COLORMAP_JET)
-
-        # Display the depth frame
-        cv2.imshow("Depth Video Stream", depth_frame_color_map)
-
-        # Quit if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            self.get_logger().info("Quitting depth display")
-            rclpy.shutdown()
-
-    def detections_2d_callback(self, msg: Detection2DArray):
-        self.detections = msg.detections
 
 
 def main(args=None):
