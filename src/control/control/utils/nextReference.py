69f4e7fa45935f4ba8e87010eab0ef3b4b54f63c
Tōkaido-Sanyō-Express <derenji@yahoo.com>
2024-11-20 05:29:52 +0000

    updates nextState to nextReference

--- src/control/control/utils/nextReference.py
+++ src/control/control/utils/nextReference.py
@@ -13,41 +13,32 @@ class nextReference:
         self.path = path
         self.index = 0
         
-    
     def initializer (self):
         self.path = None
         self.index = 0
 
-    #in progress
-    def changePath (self, newPath):
-        newIndex = self.index
-        if newIndex + 1 < newPath.size():
-            if newPath[newIndex]:
+    #naive
+    def changePath (self, newPath, newIndex):
         self.path = newPath
+        if (newIndex < newPath.size()):
+            self.index = newIndex
+        else:
+            self.index = newPath.size() - 1
 
     def changeState (self, newState):
         self.cur_state = newState
 
+    #index indicate the current path target
     def getNextReference (self):
         if index < 0:
             index = 0
         
-        currentPoint = cur_state.pos
-        
-        distanceToNext = sqrt((currentState.x - self.path[self.index].x) ** 2 + 
-                        (currentState.y - self.path[self.index].y ** 2) + 
-                        (currentState.z - self.path[self.index].z) ** 2)
+        distanceToNext = numpy.linalg.norm(currentState - self.path[self.index])
+        distanceToPrevious = numpy.linalg.norm(currentState - self.path[self.index] - 1)
 
-        distanceToPrevious = sqrt((currentState.x - self.path[self.index - 1].x) ** 2 + 
-                        (currentState.y - self.path[self.index - 1].y ** 2) + 
-                        (currentState.z - self.path[self.index - 1].z) ** 2)
-        if distanceToNext < distanceToPrevious and index < path.size() - 1:
+        if (distanceToNext < distanceToPrevious and self.index < path.size() - 1):
             index += 1
+
         return path[self.index] 
 
-    def getDistance(point1, point2):
-        return sqrt((point1.x - point2.x) ** 2 + 
-                        (point1.y - point2.y ** 2) + 
-                        (point1.z - point2.z) ** 2)
-
-    
+    #
