457a6fcbe64526e586d90f3ac2aa8de96d9e9e10
AliAhmad810 <hiimaliahmad@gmail.com>
2024-12-02 04:44:44 +0000

    i dont rlly know what this commit has

--- src/control/control/nodes/logger.py
+++ src/control/control/nodes/logger.py
@@ -10,22 +10,40 @@ Author:
     Ali Ahmad
 """
 
-import matplotlib.pyplot as plt
-
-from control.utils.state import State
-
-from nav_msgs.msg import Odometry
-
 import rclpy
 from rclpy.node import Node
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from control.utils.state import State
+from nav_msgs.msg import Odometry
 import signal
+import numpy as np
 
 class Logger(Node):
 
     def __init__(self):
         super().__init__('logger')
-        self.create_subscription(Odometry, 'odometry', self.log_path, 10)
+        self.create_subscription(Odometry, 'odometry', self.log_position, 10)
         self.positions = []
-        self.orientations = []
+
+        signal.signal(signal.SIGINT, self.handle_shutdown)
+
+    def log_position(self, msg):
+        timestamp = msg.header.stamp.sec
+        state = State.from_msg(msg)
+        self.positions.append((timestamp, state.position))
     
+    def handle_shutdown(self, signum, frame):
+        self.get_logger().info("Node shutting down. Creating plot...")
+        self.plot()
+        rclpy.shutdown()
+
+    def plot(self):
+        if not self.positions:
+            self.get_logger().info("No data to plot.")
+            return
+
+        timestamps, positions = zip(*self.positions)
+        displacements = 
+        plt.figure()
+        plt.plot(timestamps, positions)
