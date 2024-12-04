3d23393d192aa410a0455833962fafd6b041af4e
Tōkaido-Sanyō-Express <derenji@yahoo.com>
2024-12-04 05:55:14 +0000

    test_nextReference.py

--- src/control/control/utils/nextReference.py
+++ src/control/control/utils/nextReference.py
@@ -7,45 +7,49 @@ import numpy as np
 
     #!!! IS THERE A .SIZE() METHOD FOR PATH TYPE???!!!
 
-    class nextReference:
-        def __init__ (self, cur_state, path):
-            self.currentState = cur_state
-            self.path = path
-            if self.path.shape[0] == 0:
-                self.index = 0
-            else:
-                self.index = 1
-            
-        def initializer (self):
-            self.path = None
+class nextReference:
+    def __init__ (self, cur_state, path):
+        self.currentState = cur_state
+        self.path = path
+        if len(self.path) == 0:
+            self.index = 0
+        else:
             self.index = 1
-
-        #naive
-        def changePath (self, newPath, newIndex):
-            self.path = newPath
-            if (newIndex < newPath.shape[0]):
-                self.index = newIndex
-            else:
-                self.index = newPath.shape[0] - 1
-
-        def changeState (self, newState):
-            print(self.index)
-            self.currentState = newState
-
-        #index indicate the current path target
-        def getNextReference (self):
-            if self.index < 0:
-                self.index = 0
             
-            if self.path.shape[0] == 1:
-                return self.path[self.index]
+    def initializer (self):
+        self.path = None
+        if len(self.path) == 0:
+            self.index = 0
+        else:
+            self.index = 1
+        self.index = 1
 
-            else:
-                distanceToNext = np.linalg.norm(self.currentState - self.path[self.index])
-                distanceToPrevious = np.linalg.norm(self.currentState - self.path[self.index - 1])
-                if (distanceToNext < distanceToPrevious and self.index < self.path.shape[0] - 1):
-                    self.index += 1
-            return self.path[self.index] 
+    #naive
+    def changePath (self, newPath, newIndex):
+        self.path = newPath
+        if (newIndex < len(newPath)):
+            self.index = newIndex
+        else:
+            self.index = len(newPath) - 1
+
+    def changeState (self, newState):
+        print(self.index)
+        self.currentState = newState
+
+    #index indicate the current path target
+    def getNextReference (self):
+        if self.index < 0:
+            self.index = 0
+            
+        if len(self.path) == 1:
+            return self.path[self.index]
+
+        else:
+            distanceToNext = np.linalg.norm(self.currentState - self.path[self.index])
+            distanceToPrevious = np.linalg.norm(self.currentState - self.path[self.index - 1])
+            if (distanceToNext < distanceToPrevious and self.index < len(self.path) - 1):
+                self.index += 1
+        return self.path[self.index] 
             
 
             
--- src/control/test/test_nextReference.py
+++ src/control/test/test_nextReference.py
@@ -2,20 +2,26 @@ import pytest
 import numpy as np
 from control.utils.nextReference import nextReference
 
+
 def test_nextReference():
 
     curState = np.array([-3, -1, -2])
-    testPath = np.array([-2, -1, -2], [0, 0, 0], [1, 1, 1])
+    testPath = [
+        np.array([-3, -1, -2]),
+        np.array([-2, -1, -2]),
+        np.array([0, 0, 0]),
+        np.array([1, 1, 1]),
+    ]
     referencePoint = nextReference(curState, testPath)
-    
+
     print(1)
     print(referencePoint.index)
     assert referencePoint.index == 0
 
     print(referencePoint.index)
-    referencePoint.changeState(np.array([-2, -1, -2])) 
+    referencePoint.changeState(np.array([-2, -1, -2]))
     assert referencePoint.index == 1
-    
+
     print(referencePoint.index)
     referencePoint.changeState(np.array([-2, 0, 3]))
     assert referencePoint.index == 1
