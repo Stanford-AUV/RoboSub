0c8dd3e377200780ccac4b287dfec5d1d949f395
Rohan Tan Bhowmik <rohan.t.bhowmik@gmail.com>
2025-02-05 06:32:36 +0000

    [WIP] IMU

--- src/hardware/hardware/nodes/imu.py
+++ src/hardware/hardware/nodes/imu.py
@@ -4,33 +4,34 @@ from rclpy.node import Node
 from sensor_msgs.msg import Imu
 from msgs.msg import MTi200Data
 
+from geometry_msgs.msg import Vector3Stamped
 
 class IMU(Node):
 
     def __init__(self):
         super().__init__("imu")
-        self.subscription = self.create_subscription(
-            MTi200Data,  # TODO make sure data type makes sense
+        self.imu_subscription = self.create_subscription(
+            Imu,  # TODO make sure data type makes sense
             "/imu/data",
-            self.listener_callback,
+            self.imu_listener_callback,
             10,
         )
-        self.subscription  # prevent unused variable warning
 
-    def listener_callback(self, msg):
-        print(msg)
-        self.get_logger().info('I heard: "%s"' % msg)
+        self._imu_pub = self.create_publisher(Imu, "imu", 10)
+
+
+    def imu_listener_callback(self, msg):
+        self.get_logger().info(f"{msg}")
+        self._imu_pub.publish(msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
-
     imu = IMU()
     rclpy.spin(imu)
-
     imu.destroy_node()
     rclpy.shutdown()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
