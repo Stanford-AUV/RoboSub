5fece160cfabed5210a520c0edf2f0dea79135cd
Rohan Tan Bhowmik <rohan.t.bhowmik@gmail.com>
2025-05-29 21:25:13 -0700

    Light on joystick 2

--- src/manual/manual/utils/joystick.py
+++ src/manual/manual/utils/joystick.py
@@ -8,5 +8,5 @@ class JoystickState:
     ly: float = 0.0  # Left joystick Y
     rx: float = 0.0  # Right joystick X
     ry: float = 0.0  # Right joystick Y
-    light_on: bool = False
+    light_power: float = 0.0
     enabled: bool = False
