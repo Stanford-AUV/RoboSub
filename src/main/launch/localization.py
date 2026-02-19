ac21f6cd6df6621dccd674d46b36b88b0075d334
Keane Haesle <khaesle@stanford.edu>
2026-02-19 15:02:08 -0800

    removed ekf.py (not needed) and updated launch file for robot_localization and ekf.yaml

--- src/main/launch/localization.py
+++ src/main/launch/localization.py
@@ -3,9 +3,22 @@ from launch_ros.actions import Node
 import os
 
 global_params = os.path.join(os.path.dirname(__file__), "params", "global.yaml")
-
+### the robot_localization launch here can be deleted
 
 def generate_launch_description():
+
+    """
+    Generate launch description for localization launch.
+
+    This function generates a LaunchDescription object which
+    contains two Node objects: one for the "hardware" package
+    and "sensors" executable, and one for the "robot_localization"
+    package and "ekf_node" executable. Both nodes are configured
+    with the global parameters.
+
+    Returns:
+        LaunchDescription: a launch description object
+    """
     return LaunchDescription(
         [
             Node(
