1545b556b9e10487c2cace5345439494c7cf1267
malti <maltimjohn1@gmail.com>
2026-02-19 22:23:10 -0800

    updated control stuff

--- src/control/control/nodes/pid_control.py
+++ src/control/control/nodes/pid_control.py
@@ -4,11 +4,18 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import WrenchStamped
 from control.utils.pid import PID
 from control.utils.state import State
+import os
+import yaml
+import numpy as np
 
 class PIDControl(Node):
-    def __init__(self, kP_pos, kI_pos, kD_pos, kP_orie, kI_orie, 
-                 kD_orie, max_integral_pos, max_integral_orie):
+    def __init__(self, path):
+
+        self.path = path
         
+        kP_pos, kI_pos, kD_pos, kP_orie, kI_orie, \
+            kD_orie, max_integral_pos, max_integral_orie = self.get_yaml_params(self.path)
+
         self.PID = PID(kP_position=kP_pos, kI_position=kI_pos, kD_position=kD_pos,
                        kP_orientation=kP_orie, kI_orientation=kI_orie, kD_orientation=kD_orie,
                        max_integral_position=max_integral_pos, max_integral_orientation=max_integral_orie)
@@ -33,6 +40,24 @@ class PIDControl(Node):
     
     def reset(self):
         self.PID.reset()
+    
+    def get_yaml_params(self, path):
+        if not os.path.exists(path):
+            raise FileNotFoundError("Noooooo! No yaml path exists :(")
+
+        with open(self.path, "r") as f:
+            data = yaml.safe_load(f)
+
+        kP_position = np.array(data["kP_position"])
+        kD_position = np.array(data["kD_position"])
+        kI_position = np.array(data["kI_position"])
+        kP_orientation = np.array(data["kP_orientation"])
+        kD_orientation = np.array(data["kD_orientation"])
+        kI_orientation = np.array(data["kI_orientation"])
+        max_integral_position = np.array(data["max_integral_position"])
+        max_integral_orientation = np.array(data["max_integral_orientation"])
+
+        return kP_position, kI_position, kD_position, kP_orientation, kI_orientation, kD_orientation, max_integral_position, max_integral_orientation     
 
     def update_curr_state(self, msg: Odometry):
         self.curr_state = State.from_odometry_msg(msg)
@@ -47,8 +72,12 @@ class PIDControl(Node):
         self.wrench_publisher.publish(wrench_msg)
 
 def main(args=None):
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    yaml_path = os.path.join(SCRIPT_DIR, "..", "pid.yaml")
+    yaml_path = os.path.abspath(yaml_path)
+
     rclpy.init(args=args)
-    node = PIDControl()
+    node = PIDControl(yaml_path)
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
