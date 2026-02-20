afd19fbfacc5800f97222ce8f26f6da0a47568dc
malti <maltimjohn1@gmail.com>
2026-02-19 22:27:13 -0800

    added launch file for pid control

--- src/control/setup.py
+++ src/control/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 import os
 from glob import glob
@@ -11,8 +13,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
