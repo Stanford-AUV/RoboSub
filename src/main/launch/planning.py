88ad45bfcc82febe1e8e1bde3915145da9093c08
malti <maltimjohn1@gmail.com>
2026-05-31 14:33:27 -0700

    launcher

--- src/main/launch/planning.py
+++ src/main/launch/planning.py
@@ -1,11 +1,29 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 import os
+from launch.actions import DeclareLaunchArgument
+
 
 global_params = os.path.join(os.path.dirname(__file__), "params", "global.yaml")
 
 
 def generate_launch_description():
     return LaunchDescription(
-        []
+        [
+            Node(
+                package="planning",
+                executable="path_generator",
+                parameters=[global_params],
+            ),
+            Node(
+                package="planning",
+                executable="path_loader",
+                parameters=[global_params],
+            ),
+            Node(
+                package="planning",
+                executable="path_streamer",
+                parameters=[global_params],
+            ),
+        ]
     )
